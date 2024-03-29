2918bc95f0d1ddde59a26fbfe3fa0b1bb34aa621
dong <ultraraptor@naver.com>
2024-03-29 17:22:44 +0900

    feat : 목킹서버 추가

--- mockserver/mock2.js
+++ mockserver/mock2.js
@@ -1,15 +1,49 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+const { re } = require('@babel/core/lib/vendor/import-meta-resolve')
 
 const app = express()
 const port = 3000
 const args = process.argv
+const retardTimeoutMills = 30 * 60 * 1000 // 자동종료시간 : 30분
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
   logDynamicEndpoints()
 })
+
+app.get('/', (req, res) => {
+  const filePath = jsonFilePath()
+  console.log('DB >', filePath)
+  if (!filePath) {
+    return res.status(404).send(' ERROR!! File path is required')
+  }
+  const jsonData = readFromFile(filePath)
+  if (jsonData) {
+    allowAnyOrigins(res)
+    res.json(jsonData)
+  } else {
+    res.status(500).send('ERROR!! reading JSON file')
+  }
+})
+
+app.get('/:dataName', (req, res) => {
+  const { dataName } = req.params
+  const filePath = jsonFilePath()
+  if (!filePath) {
+    return res.status(404).send(' ERROR!! File path is required')
+  }
+  const jsonData = readFromFile(filePath)
+  if (jsonData && jsonData[dataName]) {
+    allowAnyOrigins(res)
+    res.json(jsonData[dataName])
+  } else {
+    res.status(500).send(`ERROR!! ${dataName} data not found`)
+  }
+})
+
+//--
 
 function jsonFilePath() {
   return path.join(__dirname, args[2])
@@ -25,7 +59,6 @@ function readFromFile(filePath) {
   }
 }
 
-// 동적 엔드포인트 로그
 function logDynamicEndpoints() {
   const filePath = jsonFilePath()
   if (!filePath) {
@@ -42,30 +75,11 @@ function logDynamicEndpoints() {
   })
 }
 
-app.get('/', (req, res) => {
-  const filePath = jsonFilePath()
-  console.log('DB >', filePath)
-  if (!filePath) {
-    return res.status(404).send(' ERROR!! File path is required')
-  }
-  const jsonData = readFromFile(filePath)
-  if (jsonData) {
-    res.json(jsonData)
-  } else {
-    res.status(500).send('ERROR!! reading JSON file')
-  }
-})
+function allowAnyOrigins(res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+}
 
-app.get('/:dataName', (req, res) => {
-  const { dataName } = req.params
-  const filePath = jsonFilePath()
-  if (!filePath) {
-    return res.status(404).send(' ERROR!! File path is required')
-  }
-  const jsonData = readFromFile(filePath)
-  if (jsonData && jsonData[dataName]) {
-    res.json(jsonData[dataName])
-  } else {
-    res.status(500).send(`ERROR!! ${dataName} data not found`)
-  }
-})
+setTimeout(() => {
+  console.log('elapsed... Closing server')
+  app.close()
+}, retardTimeoutMills)
