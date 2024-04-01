343824a20dbeab1d29a4fc98242395e46cd64068
dong <ultraraptor@naver.com>
2024-04-01 09:25:06 +0900

    feat : 중간저장

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,30 @@
   <div id='app'>
     <header>
       <AppHeader/>
-      <TabBar />
-  </header>
-  <body>
-    <!--    <MenuBar />-->
-    <DraggableTab/>
-    <router-view />
-  </body>
-  <footer>
-    <AppFooter/>
-  </footer>
+      <TabBar/>
+    </header>
+    <body>
+      <router-view/>
+    </body>
+    <footer>
+      <AppFooter />
+    </footer>
 
   </div>
 </template>
+
+
 
 
 <script>
 import TabBar from '@/layout/TabBar.vue'
 import AppFooter from '@/layout/AppFooter.vue'
 import AppHeader from '@/layout/AppHeader.vue'
-import DraggableTab from '@/layout/poc/DraggableTab.vue'
-// import MenuBar from '@/layout/MenuBar.vue'
+// import DraggableTab from '@/layout/poc/DraggableTab.vue'
 
 export default {
   name: 'App',
-  components: { DraggableTab, AppHeader, AppFooter, TabBar }
+  components: { AppHeader, AppFooter, TabBar }
   /*layout: {  <-- Vue 프로젝트 생성 시 존재하는 기본 코드 제거
     HelloWorld
   }*/
